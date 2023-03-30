4ee61beb52398cc1e710ea30666aa6713262a2e8
Pritush Kr Singh <mynameispritush.15@gmail.com>
2023-03-30 22:26:08 +0530

    Done with the basic wallk through of the simple code

---
+++ Basic_Understanding_ofeverything_each_line.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+//anything begins with the hash is a preprocess statement
+//the first thing the compiler does when it gets a file isto preprocess all the preprocess statements 
+
+int main(){
+    // this << these operators are basically a function 
+    //cout is basically what we want to show on the screen 
+    std:: cout<<"HElloe" << std:: endl;
+    //this get function waits for the usser to press the enter button 
+    std:: cin.get();
+}
+//the main function is the special case for the which we dont have to return a typr value
+
+/// the cpp files only gets compiled the header files doesnt gets compiled at all 
