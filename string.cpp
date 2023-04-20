17c4b3fae8cbf3f5c10b9882120cd63eda8707ef
Pritush Kr Singh <mynameispritush.15@gmail.com>
2023-04-20 23:03:48 +0530

    pushback and cin.ignore()

--- string.cpp
+++ string.cpp
@@ -2,5 +2,38 @@
 using namespace std;
 
 int main(){
-    cout << "Hell";
+    // declare them with the string var name
+    string s,c,d;
+    cin >> s;
+    cin >> c;
+    //adding
+    cout << s+c << endl;
+
+    //equating
+    if (c==s){
+        cout << "Equal";
+    }
+    else{
+        cout << "NEqual";
+    }
+
+    // Accessing a particular character and changing
+    c[0]='a';
+    cout << endl <<c[0];
+    // these are characters so u cant reassign them using double qoutes (! c[0]="a")
+
+    // getting the size 
+    cout << endl << c.size()<<endl;
+    
+    
+    // if u want the same input with the spaces and other things use getline function
+    // getline() reads from the input, there is a newline character left in the input stream, so it doesn't read your c-string. Use cin. ignore() before calling getline() .
+    //The cin before one ends on slashes or the new line so the getline does not ignore the slashes or the newline so we use ignore function.
+    cin.ignore();
+    getline(cin,d);
+    cout << d;
+
+    //  to append a character in the last of the string use push_back
+    s.push_back(s[0]);
+    cout << endl << s;
 }
