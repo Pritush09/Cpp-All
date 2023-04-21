c135f3d89aa7a9a6c6baa4b11325e66a3628dd8d
Pritush Kr Singh <mynameispritush.15@gmail.com>
2023-04-21 12:12:25 +0530

    done with single pointer

--- pointers.cpp
+++ pointers.cpp
@@ -4,9 +4,22 @@ using namespace std;
 int main(){
     int a = 1;
     int *p_a = &a;
-    cout << p_a; // this gives the hexadecimal number of the address 
-    cout << *p_a; // gives the value of the address
+    cout << p_a << endl; // this gives the hexadecimal number of the address 
+    cout << *p_a<<endl; // gives the value of the address
+    *p_a = 5;
+    // this channges the value at the address
+    cout << *p_a<< endl;
 
+    // changing the address 
+    int *changed_address = p_a+1;
+    cout << p_a+1 << endl;
+    cout << *changed_address<<endl;// this the stuff present at that point
+    // the unchanged p_a
+    cout << *p_a << endl;
+
+    // Double pointer
     
+
+
 
 }
