aef9fe53fedb5d6b7664ee55be92779b76586f2e
Pritush Kr Singh <mynameispritush.15@gmail.com>
2023-04-21 12:01:37 +0530

    starting with the pointers

---
+++ pointers.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 1;
+    int *p_a = &a;
+    cout << p_a; // this gives the hexadecimal number of the address 
+    cout << *p_a; // gives the value of the address
+
+    
+
+}
