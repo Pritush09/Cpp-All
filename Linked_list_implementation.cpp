a076a79ca6df94257c7e4aa0bb84c67b88f6bbe4
Pritush Kr Singh <mynameispritush.15@gmail.com>
2023-04-22 11:23:42 +0530

    Linked list basic

---
+++ Linked_list_implementation.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* link;
+};
+
+// to create the dynamically allocated node we use {new Node()} in c++
+
+int main(){
+    
+}
